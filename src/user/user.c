5c40c3607eba14c5064ff77495f2049be6fd5c2f
shibuyanorailgun <shibuyanorailgun@gmail.com>
2014-04-14 21:56:25 +0800

    增加了识别以太网设备的功能

--- src/user/user.c
+++ src/user/user.c
@@ -18,8 +18,12 @@
 #include "user.h"
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <ifaddrs.h>
 
 #define USER_FILE_PATH "/etc/MyH3C/user.conf"
 
@@ -29,14 +33,46 @@ static user_t create_user()
 {
   FILE *pf = fopen(USER_FILE_PATH, "w");
   user_t user;
-  printf("%s:\n", "Enter username");
+  printf("%s:\n", "Enter your netid username");
   scanf("%s", user.username);
   printf("%s:\n", "Enter password");
   scanf("%s", user.password);
-  printf("%s:\n",
-      "Enter ethernet device name, for newbies, you can simply Enter `eth0' if "
-      "you don't know what it means");
-  scanf("%s", user.device_name);
+
+  struct ifaddrs *adrs = NULL;
+  if (0 != getifaddrs(&adrs)) {
+    perror("Fail to get network interface list, you might need "
+        "select the interface by yourself");
+    printf("%s:\n",
+        "Enter ethernet device name, for newbies, you can simply "
+        "enter `eth0' if you don't know what it means");
+    scanf("%s", user.device_name);
+  } else {
+    perror("Initialize ethernet interface list");
+    errno = 0; // FIXME: what caused this?
+    struct ifaddrs *cur = NULL;
+    for (cur = adrs; cur != NULL; cur = cur->ifa_next) {
+      if (cur->ifa_addr->sa_family != AF_PACKET)
+        continue;
+      printf("AF_PACKET network interface found: %s\n",
+          cur->ifa_name);
+      if (!strcasestr(cur->ifa_name, "wlan") // Suggested interface
+          && !strcasestr(cur->ifa_name, "lo")) {
+        sprintf(user.device_name, "%s", cur->ifa_name);
+      }
+    }
+    while (1) {
+      if (user.device_name[0] != '\0') {
+        printf("Interface %s suggested, are you sure?<Y/N>\n",
+            user.device_name);
+        char ch;
+        scanf(" %c", &ch);
+        if ('Y' == toupper(ch))
+          break;
+      }
+      printf("Enter your interface name\n");
+      scanf("%s", user.device_name);
+    }
+  }
   fprintf(pf, USER_PATTERN, user.username, user.password, user.device_name);
   fclose(pf);
   return user;
